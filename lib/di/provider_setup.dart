9f0e232c81046b1e9dfeaadcab1b011c5aee4cbb
kimnamju <kimnamju0920@gmail.com>
2022-08-31 17:45:34 +0900

    [AM-200] feat: 경험치 repository, usecase DI

--- lib/di/provider_setup.dart
+++ lib/di/provider_setup.dart
@@ -1,10 +1,15 @@
+import 'package:amond/data/repository/exp_repository_impl.dart';
 import 'package:amond/data/repository/member_repository_impl.dart';
+import 'package:amond/data/source/network/api/exp_api.dart';
 import 'package:amond/data/source/network/api/member_api.dart';
-import 'package:amond/domain/repositories/member_repository.dart';
+import 'package:amond/domain/usecases/exp/exp_use_cases.dart';
+import 'package:amond/domain/usecases/exp/get_exp.dart';
 import 'package:amond/domain/usecases/member/member_use_cases.dart';
 import 'package:amond/presentation/controllers/auth_controller.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
+
+import '../domain/usecases/exp/change_exp.dart';
 
 List<SingleChildWidget> globalProviders = [
   ...independentModels,
@@ -13,17 +18,28 @@ List<SingleChildWidget> globalProviders = [
 ];
 
 List<SingleChildWidget> independentModels = [
-  Provider(create: (_) => MemberApi()),
+  Provider<MemberApi>(create: (_) => MemberApi()),
+  Provider<ExpApi>(create: (_) => ExpApi()),
 ];
 
 List<SingleChildWidget> dependentModels = [
   ProxyProvider<MemberApi, MemberRepositoryImpl>(
       update: (_, api, __) => MemberRepositoryImpl(api)),
+  ProxyProvider<ExpApi, ExpRepositoryImpl>(
+    update: (_, api, __) => ExpRepositoryImpl(api),
+  ),
   ProxyProvider<MemberRepositoryImpl, MemberUseCases>(
     update: (_, repository, __) => MemberUseCases(repository),
+  ),
+  ProxyProvider<ExpRepositoryImpl, ExpUseCases>(
+    update: (_, repository, __) => ExpUseCases(
+      changeExp: ChangeExp(repository),
+      getExp: GetExp(repository),
+    ),
   )
 ];
 
 List<SingleChildWidget> viewModels = [
- Provider<AuthController>(create: (_) => AuthController(_.read<MemberUseCases>())),
+  Provider<AuthController>(
+      create: (_) => AuthController(_.read<MemberUseCases>())),
 ];
