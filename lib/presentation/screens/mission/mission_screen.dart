467cf5f5e113e8716e2f84259fb2471807db0a1e
kimnamju <kimnamju0920@gmail.com>
2022-09-14 23:26:18 +0900

    [AM-232, AM-233] style: 미션 카드 디자인 수정

--- lib/presentation/screens/mission/mission_screen.dart
+++ lib/presentation/screens/mission/mission_screen.dart
@@ -14,12 +14,23 @@ class MissionScreen extends StatelessWidget {
           itemBuilder: (context, index) => Container(
             // 카드 간의 상하 간격
             margin: const EdgeInsets.symmetric(vertical: 4),
-            child: Card(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                color: backgroundColor,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.6),
+                    blurRadius: 6,
+                    offset: const Offset(4, 4),
+                  )
+                ]
+              ),
               child: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 4),
                 child: ListTile(
                   leading: Icon(Icons.water_drop, color: Colors.blue,),
-                  title: Text("티끌모아 태산", style: TextStyle(fontSize: 20),),
+                  title: Text("티끌모아 태산   +8XP", style: TextStyle(fontSize: 20),),
                   subtitle: Text("물을 한 방울이라도 아끼는 것이 지구에 도움이 된다는 것을 아시나요? 양치할 때 겸사겸사 물을 받아놓고 해봐요!"),
                   trailing: index == 1 ?  Icon(Icons.check_box_outline_blank) : Icon(Icons.check_box_outlined, color: Colors.green,),
                 ),
