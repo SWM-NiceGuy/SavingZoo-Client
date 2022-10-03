8818bd3bc1820b73df0f8ef47234e1ab49196304
kimnamju <kimnamju0920@gmail.com>
2022-10-03 14:49:45 +0900

    [AM-260] feat: 미션 내역 카드 컴포넌트화

--- lib/presentation/screens/mission/mission_history_screen.dart
+++ lib/presentation/screens/mission/mission_history_screen.dart
@@ -1,6 +1,6 @@
 import 'package:amond/domain/models/mission_history.dart';
 import 'package:amond/domain/models/mission_state.dart';
-import 'package:amond/presentation/screens/mission/util/get_history_text_by_state.dart';
+import 'package:amond/presentation/screens/mission/components/mission_history_card.dart';
 import 'package:amond/ui/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -27,43 +27,10 @@ class MissionHistoryScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemCount: 3,
-        itemBuilder: (context, index) => Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Row(
-                children: [
-                  // 수행 시간
-                  Text('${histories[index].date.month}.${histories[index].date.day}'),
-                  const SizedBox(width: 16),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // 기록 내용
-                      Text('${histories[index].type}${histories[index].missionName}'),
-                      // 성공 여부
-                      getHistoryTextByState(histories[index].state) 
-                    ],
-                  ),
-                  const Spacer(),
-                  // 보상
-                  if (histories[index].state == MissionState.accepted)
-                  Text('+ ${histories[index].reward}XP',
-                      style: const TextStyle(
-                          fontSize: 18,
-                          color: successColor,
-                          fontWeight: FontWeight.bold)),
-                ],
-              ),
-            ),
-            Container(
-              height: 1,
-              width: double.infinity,
-              color: const Color(0xffd7d7d7),
-            )
-          ],
-        ),
+        itemBuilder: (context, index) => HistoryCard(history: histories[index]),
       ),
     );
   }
 }
+
+
