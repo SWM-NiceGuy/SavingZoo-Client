23a865067b96f4e181b72964ec2db9d1dc51db66
jinsubaekdev <jinsu350@gmail.com>
2022-11-11 10:35:08 +0900

    [AM-362] feat: 페이스북 SDK 적용
    안드로이드는 적용 및 테스트 완료되었으나, iOS는 적용만 하고 테스트에서 동작을 안하는 상황 발생

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,81 @@
 import UIKit
 import Flutter
 
+//@UIApplicationMain
+//@objc class AppDelegate: FlutterAppDelegate {
+//  override func application(
+//    _ application: UIApplication,
+//    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//  ) -> Bool {
+//    GeneratedPluginRegistrant.register(with: self)
+//    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//  }
+//}
+
+import UIKit
+import Flutter
+import FacebookCore
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
+    override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+) -> Bool {
+    ApplicationDelegate.shared.application(
+        application,
+        didFinishLaunchingWithOptions: launchOptions
+    )
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 }
+
+    override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+) -> Bool {
+    ApplicationDelegate.shared.application(
+        app,
+        open: url,
+        sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+        annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+    )
+}
+}
+
+
+
+// 페이스북 SDK 설치를 위해 아래 코드로 대체
+//import UIKit
+//import Flutter
+//import FacebookCore
+//
+//@UIApplicationMain
+//class AppDelegate: UIResponder, UIApplicationDelegate {
+//func application(
+//    _ application: UIApplication,
+//    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//) -> Bool {
+//    ApplicationDelegate.shared.application(
+//        application,
+//        didFinishLaunchingWithOptions: launchOptions
+//    )
+//
+//    return true
+//}
+//
+//func application(
+//    _ app: UIApplication,
+//    open url: URL,
+//    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+//) -> Bool {
+//    ApplicationDelegate.shared.application(
+//        app,
+//        open: url,
+//        sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+//        annotation: options[UIApplication.OpenURLOptionsKey.annotation]
+//    )
+//}
+//}
